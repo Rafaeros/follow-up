5046bc1b7cb14efa35aac97628d93d3ee8d16b6a
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2023-08-12 11:50:01 -0300

    Finalized to switch openpyxl to pandas filterr

--- pedidosAtraso.py
+++ pedidosAtraso.py
@@ -6,8 +6,8 @@ from datetime import timezone
 
 # Pegando data de hoje
 data_hoje = dt.datetime.now()
-print(data_hoje)
 # Criando a classe para fornecedores / pedidos / emails
+
 
 class Fornecedor():
     def __init__(self, Nome, Email):
@@ -31,19 +31,33 @@ class Fornecedor():
         print("------------------------")
 
 
-# Carregando Planilha
+Pedidos = pd.read_excel('EntregasPendentes10_07_2023.xlsx')
+
+Pedidos = Pedidos[Pedidos['Situação'] != 'Envio pendente']
+
+Pedidos = Pedidos[Pedidos['Nacionalidade'] == 'Brasil']
+
+valoresRateio = ['MATERIA-PRIMA',
+                 'MATERIA PRIMA INDUSTRIALIZAÇÃO', 'MATERIAL DE USO E CONSUMO']
+Pedidos = Pedidos[Pedidos['Rateio'].isin(valoresRateio)]
+
+print(Pedidos)
+
+Pedidos.to_excel('TestePandas.xlsx')
+
+""" # Carregando Planilha
 ws = op.load_workbook('./EntregasPendentes10_07_2023.xlsx')
 planilha_ativa = ws.active
 ult_linha_planilha = planilha_ativa.max_row
 
 # Vendo todos pedidos que estão atrasados
-""" def pegandoPedidosAtrasados(database):
+def pegandoPedidosAtrasados(database):
     i=0
     fill_atrasado = Pattern
     ultima_linha = database.max_row
     datas_antigas = [planilha_ativa.cell(row=i, column=2).value for i in range(2,ultima_linha):]
     
-    if(planilha_ativa.cell(row=i, column=2)<data_hoje: planilha_ativa[f"B.{celula.row}"].fill = PatternFill(start_color) """
+    if(planilha_ativa.cell(row=i, column=2)<data_hoje: planilha_ativa[f"B.{celula.row}"].fill = PatternFill(start_color)
 
 
 # Limpando planilha
@@ -63,20 +77,16 @@ for celula in planilha_ativa['M']:
         planilha_ativa.delete_rows(linha_celula)
 
 
-ws.save("PedidoAtraso.xlsx")
+ws.save("PedidoAtraso.xlsx") """
 
-tabelapd = pd.read_excel("./PedidoAtraso.xlsx")
+""" tabelapd = pd.read_excel("./PedidoAtraso.xlsx")
 
 # Puxando fornecedores sem duplicatas
 fornecedores = tabelapd.loc[:, ['Fornecedor']].drop_duplicates(
     subset="Fornecedor", keep="first").values.tolist()
-""" print(fornecedores)
-print("total fornecedores: ", len(fornecedores)) """
 
 pedidosTMF = tabelapd.loc[tabelapd['Fornecedor'] == 'TMF COMPONENTES ELETRO EL ETRONICOS LTDA', [
     "Neg.", "Data de entrega", "Cod.", "Material", "Faltam"]].reset_index()
-print(pedidosTMF)
-
 pedidosTMF.pop(pedidosTMF.columns[0])
 
 pedidosTMF.index += 1
@@ -87,6 +97,4 @@ pedidosTMF['Data de entrega'] = pd.to_datetime(
 pedidosTMF['Data de entrega'] = pedidosTMF["Data de entrega"].dt.strftime(
     "%d/%m/%Y   ")
 
-print(pedidosTMF)
-
-pedidosTMF.to_excel('PedidosTMF.xlsx')
+pedidosTMF.to_excel('PedidosTMF.xlsx') """
