a0314588e2c337c7c76b1b8cde5b9ee0cf2c2307
Rafael Costa <rafaeladriancordeirocosta@gmail.com>
2023-09-17 14:02:41 -0300

    Changed the code language to english

--- pedidosAtraso.py
+++ pedidosAtraso.py
@@ -1,78 +1,96 @@
 import pandas as pd
 import datetime as dt
+from datetime import timedelta
 import time
 import win32com.client as win32
-# from tkinter import *
-# from tkinter import Tk, Button, filedialog
 import tkinter as tk
 from tkinter import filedialog
-import chardet
 
 
-# Pegando data de hoje
-data_hoje = dt.datetime.now()
-data_hoje = data_hoje.strftime('%d/%m/%Y')
-print(type(data_hoje))
-print(data_hoje)
-# Criando a classe para fornecedores / pedidos / emails
+# Getting today date
+today_date = dt.datetime.now()
+
+# Email style
+style = """
+<style>
+* {
+padding: 5px;
+}
+
+thead {
+  text-align: center;
+  background-color: cadetblue;
+}
+
+tr, th,td {
+  text-align: center;
+  justify-content: center;
+}
+
+td:nth-child(5) {
+  text-align: left;
+  background-color: red;
+}
+</style>
+"""
 
 
-class Fornecedor():
-    def __init__(self, Nome, Email, TotalPedidos):
-        self.Nome = Nome
+class Supplier():
+    def __init__(self, Name, Email, TotalOrders):
+        self.Name = Name
         self.Email = Email
-        self.TotalPedidos = TotalPedidos
+        self.TotalOrders = TotalOrders
 
 
 def userInterface():
-    janela = tk.Tk()
-    janela.title("FollowUp F&K")
-    janela.geometry("350x400")
-    janela.resizable(False, False)
+    window = tk.Tk()
+    window.title("FollowUp F&K")
+    window.geometry("350x400")
+    window.resizable(False, False)
     string_path = tk.StringVar()
     string_path.set("Arquivo Selecionado")
 
     def add_email_file():
-        global email_file_path
-        email_file_path = filedialog.askopenfilenames()
-        print(email_file_path)
-        email_file_path = "".join(email_file_path)
+        global email_data_filepath
+        email_data_filepath = filedialog.askopenfilenames()
+        print(email_data_filepath)
+        email_data_filepath = "".join(email_data_filepath)
 
     def add_file():
-        global file_path
-        file_path = filedialog.askopenfilenames()
-        print(file_path)
-        file_path = "".join(file_path)
-        string_path.set(file_path)
+        global orders_data_filepath
+        orders_data_filepath = filedialog.askopenfilenames()
+        print(orders_data_filepath)
+        orders_data_filepath = "".join(orders_data_filepath)
+        string_path.set(orders_data_filepath)
 
-    step_1 = tk.Label(janela, text="1° Passo")
+    step_1 = tk.Label(window, text="1° Passo")
     step_1.pack(pady=10)
 
     emailDialogButton = tk.Button(
-        janela, text="Adicionar Arquivo C/ Emails", command=add_email_file)
+        window, text="Adicionar Arquivo C/ Emails", command=add_email_file)
     emailDialogButton.pack(pady=10)
 
-    step_2 = tk.Label(janela, text="2° Passo")
+    step_2 = tk.Label(window, text="2° Passo")
     step_2.pack(pady=10)
 
     fileDialogButton = tk.Button(
-        janela, text="Adicionar Arquivo C/ Pedidos", command=add_file)
+        window, text="Adicionar Arquivo C/ Pedidos", command=add_file)
     fileDialogButton.pack(pady=10)
 
-    selectlabel = tk.Label(janela, textvariable=string_path)
+    selectlabel = tk.Label(window, textvariable=string_path)
     selectlabel
 
-    step_3 = tk.Label(janela, text="3° Passo")
+    step_3 = tk.Label(window, text="3° Passo")
     step_3.pack(pady=30)
 
-    send_emails = tk.Button(janela, text="Enviar Emails",
-                            command=data_push_pandas)
+    send_emails = tk.Button(window, text="Enviar Emails",
+                            command=data_push)
     send_emails.pack(pady=10)
 
-    janela.mainloop()
+    window.mainloop()
 
 
-def formatar_dados(Orders):
+def format_data(Orders):
     Orders.pop(Orders.columns[0])
 
     Orders.index += 1
@@ -84,41 +102,81 @@ def formatar_dados(Orders):
         "%d/%m/%Y   ")
 
 
-def data_push_pandas():
+def data_push():
+    suppliers_data = pd.read_excel(email_data_filepath)
+    emails_data = suppliers_data[["Nome", "Email"]]
 
-    suppliers_data = pd.read_excel(email_file_path)
-    email_data = suppliers_data[["Nome", "Email"]]
+    total_orders = pd.read_excel(orders_data_filepath)
+    total_orders = total_orders[total_orders['Situação'] != 'Envio pendente']
+    total_orders = total_orders[total_orders['Nacionalidade'] == 'Brasil']
+    MP_filter = ['MATERIA-PRIMA', 'MATERIA PRIMA INDUSTRIALIZAÇÃO',
+                 'MATERIAL DE USO E CONSUMO']
+    total_orders = total_orders[total_orders['Rateio'].isin(MP_filter)]
 
-    Pedidos = pd.read_excel(file_path)
-    Pedidos = Pedidos[Pedidos['Situação'] != 'Envio pendente']
-    Pedidos = Pedidos[Pedidos['Nacionalidade'] == 'Brasil']
-    valoresRateio = ['MATERIA-PRIMA',
-                     'MATERIA PRIMA INDUSTRIALIZAÇÃO', 'MATERIAL DE USO E CONSUMO']
-    Pedidos = Pedidos[Pedidos['Rateio'].isin(valoresRateio)]
-    Pedidos = Pedidos[Pedidos['Data de entrega'] < data_hoje]
+    total_orders['Data de entrega'] = pd.to_datetime(
+        total_orders['Data de entrega'], format='%d/%m/%Y')
 
-    print(Pedidos)
+    # Late Orders for corrective treatment
+    total_late_orders = total_orders[total_orders['Data de entrega'] < today_date]
 
-    Pedidos['Fornecedor'].to_string()
-    fornecedores = Pedidos.loc[:, ['Fornecedor']].drop_duplicates(
-        subset="Fornecedor", keep="first").values.tolist()
+    # Ten days ahead Orders for preventive preventive treatment
+    date_tenDaysAhead = today_date + timedelta(days=11)
+    dateMask = (total_orders['Data de entrega'] > today_date) & (
+        total_orders['Data de entrega'] <= date_tenDaysAhead)
+    orders_tenDaysAhead = total_orders.loc[dateMask]
 
-    lista_fornecedores = []
-    for fornecedor in fornecedores:
-        lateOrders = Pedidos.loc[Pedidos['Fornecedor'] == fornecedor[0], [
-            "Neg.", "Data de entrega", "Fornecedor", "Cod.", "Material", "Faltam"]].reset_index()
-        lateOrders.index.name = "N"
-        formatar_dados(lateOrders)
+    if (total_orders['Data de entrega'] < today_date):
+        total_late_orders['Fornecedor'].to_string()
 
-        current_email = email_data.loc[email_data['Nome']
-                                       == fornecedor[0], ["Email"]]
+        suppliers = total_late_orders.loc[:, ['Fornecedor']].drop_duplicates(
+            subset="Fornecedor", keep="first").values.tolist()
 
-        print(current_email)
+        suppliers_list = []
+        for supplier in suppliers:
+            lateOrders = total_late_orders.loc[total_late_orders['Fornecedor'] == supplier[0], [
+                "Neg.", "Data de entrega", "Fornecedor", "Cod.", "Material", "Faltam"]].reset_index()
+            lateOrders.index.name = "N"
+            format_data(lateOrders)
 
-        lista_fornecedores.append(Fornecedor(
-            fornecedor[0], f"", lateOrders))
+            current_email = emails_data.loc[emails_data['Nome'] == supplier[0], [
+                "Email"]]
+            suppliers_list.append(
+                Supplier(supplier[0], f"{current_email}", lateOrders))
+            # Supplier(Name, Email, Totalorders)
+
+        sendEmail(suppliers_list)
+
+    # Comando para gerar arquivos excel bom base nos total_late_orders e nomes de cada fornecedor
+    # PedidosAtrasados.to_excel(f'total_late_orders{fornecedor[0]}.xlsx')
 
 
-    # Comando para gerar arquivos excel bom base nos pedidos e nomes de cada fornecedor
-    # PedidosAtrasados.to_excel(f'Pedidos{fornecedor[0]}.xlsx')
+def sendEmail(suppliersList):
+    outlook = win32.Dispatch("Outlook.Application")
+    for supplier in suppliersList:
+        lateOrdersHTML = supplier.TotalOrders.to_html(
+            col_space=50, justify='center')
+        html_body = f"""
+        <!DOCTYPE html>
+        <html>
+        <head>
+            {style}
+        </head>
+        <body>
+            <h1>Olá:{supplier.Nome}</h1>
+            <h2>Favor validar esses pedidos que constam em atraso em nosso sistema: </h2>
+            {lateOrdersHTML}
+        </body>
+        </html>
+        """
+        print(html_body)
+        email = outlook.CreateItem(0)
+        time.sleep(1)
+        email.To = 'rafaelzinhobr159@gmail.com'
+        email.Subject = f"Pedidos atrasados {supplier.Name}"
+        email.HTMLBody = (html_body)
+        email.Send()
+        print(f"Email enviado: {supplier.Name}")
+        time.sleep(2)
+
+
 userInterface()
